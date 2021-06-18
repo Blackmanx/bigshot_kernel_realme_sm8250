d909181b3f5ef39c49d1b67156a478b628d8c5c2
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2021-06-18 13:41:46 +0300

    cfg80211: set custom regdomain after wiphy registration
    commit 1b7b3ac8ff3317cdcf07a1c413de9bdb68019c2b upstream.
    
    We used to set regulatory info before the registration of
    the device and then the regulatory info didn't get set, because
    the device isn't registered so there isn't a device to set the
    regulatory info for. So set the regulatory info after the device
    registration.
    Call reg_process_self_managed_hints() once again after the device
    registration because it does nothing before it.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20210618133832.c96eadcffe80.I86799c2c866b5610b4cf91115c21d8ceb525c5aa@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -4,7 +4,7 @@
  * Copyright 2006-2010		Johannes Berg <johannes@sipsolutions.net>
  * Copyright 2013-2014  Intel Mobile Communications GmbH
  * Copyright 2015-2017	Intel Deutschland GmbH
- * Copyright (C) 2018-2020 Intel Corporation
+ * Copyright (C) 2018-2021 Intel Corporation
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
@@ -866,6 +866,9 @@ int wiphy_register(struct wiphy *wiphy)
 
 	cfg80211_debugfs_rdev_add(rdev);
 	nl80211_notify_wiphy(rdev, NL80211_CMD_NEW_WIPHY);
+
+	/* set up regulatory info */
+	wiphy_regulatory_register(wiphy);
 
 	if (wiphy->regulatory_flags & REGULATORY_CUSTOM_REG) {
 		struct regulatory_request request;
