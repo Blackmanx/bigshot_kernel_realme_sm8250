641e22af8f4dd6730c27cc9076e4685aa7613910
Blackmanx <blackman@sajl.tk>
2022-12-26 18:45:31 +0100

    drivers: mailbox: add missing params

--- drivers/mailbox/mailbox.c
+++ drivers/mailbox/mailbox.c
@@ -91,7 +91,7 @@ exit:
 static void msg_submit(struct mbox_chan *chan)
 {
 	int err = 0;
-
+	unsigned long flags;
 	/*
 	 * If the controller returns -EAGAIN, then it means, our spinlock
 	 * here is preventing the controller from receiving its interrupt,
