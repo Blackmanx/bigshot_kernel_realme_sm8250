15e04b54737bb3c0619e030f64497367322738c6
Blackmanx <blackman@sajl.tk>
2023-02-06 21:23:25 +0100

    Revert "qmi_rmnet: Do not use alarm timer for DFC"
    This reverts commit a8bbde0661002e9888bf290fa3ecc7f8c31ed286.

--- drivers/soc/qcom/qmi_rmnet_i.h
+++ drivers/soc/qcom/qmi_rmnet_i.h
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2018-2020, 2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _RMNET_QMI_I_H
@@ -99,7 +98,6 @@ struct qmi_info {
 	bool ps_enabled;
 	bool dl_msg_active;
 	bool ps_ignore_grant;
-	int ps_ext;
 };
 
 enum data_ep_type_enum_v01 {
@@ -217,7 +215,6 @@ wda_qmi_client_init(void *port, struct svc_info *psvc, struct qmi_info *qmi);
 void wda_qmi_client_exit(void *wda_data);
 int wda_set_powersave_mode(void *wda_data, u8 enable);
 void qmi_rmnet_flush_ps_wq(void);
-int dfc_qmap_set_powersave(u8 enable, u8 num_bearers, u8 *bearer_id);
 #else
 static inline int
 wda_qmi_client_init(void *port, struct svc_info *psvc, struct qmi_info *qmi)
@@ -235,11 +232,6 @@ static inline int wda_set_powersave_mode(void *wda_data, u8 enable)
 }
 static inline void qmi_rmnet_flush_ps_wq(void)
 {
-}
-static inline int dfc_qmap_set_powersave(u8 enable, u8 num_bearers,
-		u8 *bearer_id)
-{
-	return -EINVAL;
 }
 #endif
 #endif /*_RMNET_QMI_I_H*/
